4db595daf957e38e9a8e0971c2d2d1994f1c07b7
Julian <juliancano.dev@gmail.com>
2021-02-11 00:56:41 -0500

    first commit

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 20.0f;
+    [SerializeField] private float turnSpeed = 10.0f;
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime *  horizontalInput);
+    }
+}
