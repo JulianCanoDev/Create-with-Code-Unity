1770eb9fcdb796f997a9767ddef6cf28ef07cbff
Julian <juliancano.dev@gmail.com>
2021-02-11 01:53:40 -0500

    Challenge 1 plane

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,9 @@
 
 public class PlayerController : MonoBehaviour
 {
+    // Privated Variables
     [SerializeField] private float speed = 20.0f;
-    [SerializeField] private float turnSpeed = 10.0f;
+    [SerializeField] private float turnSpeed = 20.0f;
     [SerializeField] private float horizontalInput;
     [SerializeField] private float forwardInput;
     // Start is called before the first frame update
@@ -15,11 +16,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // This is where we get player inputs
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        // Move the vehicle forward
+        // Move the vehicle forward based in Vertical Input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // Rotate the vehicle forward based in Horizontal Input
         transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime *  horizontalInput);
     }
 }
